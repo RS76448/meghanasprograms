e5986b8b9eddcfa015597545bfb6cc36ebe016b0
Rohit sharma <amrohitk70@gmail.com>
2025-07-15 22:18:36 +0530

    lk

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://naszulahxzhrvqodfsai.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hc3p1bGFoeHpocnZxb2Rmc2FpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1OTc4NzUsImV4cCI6MjA2ODE3Mzg3NX0.IKz7g5LDqZRQBjn3P-2sqms1CKCzUk4xH2cTUYd7Hsw'
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY  || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hc3p1bGFoeHpocnZxb2Rmc2FpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1OTc4NzUsImV4cCI6MjA2ODE3Mzg3NX0.IKz7g5LDqZRQBjn3P-2sqms1CKCzUk4xH2cTUYd7Hsw'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
